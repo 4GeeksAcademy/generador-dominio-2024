35ede16286801c352e534607fca7d2b527824a10
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-11-26 07:52:12 +0000

    creo codigo js, para iterar 4 arrays y crear un generador de dominio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronombres = ["the", "our"];
+  let adjetivos = ["great", "big"];
+  let sustantivos = ["jogger", "racoon"];
+  let dominios = [".io", ".com", ".es"];
   console.log("Hello Rigo from the console!");
+
+  for (i = 0; i < pronombres.length; i++) {
+    for (i = 0; i < adjetivos.length; i++) {
+      for (i = 0; i < sustantivos.length; i++) {
+        for (i = 0; i < dominios.length; i++) {
+          console.log(`${pronombres},${adjetivos},${sustantivos},${dominios}`);
+        }
+      }
+    }
+  }
 };
