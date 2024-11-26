e8263d9c5025eab6780d6c237499b519b80fddff
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-11-26 17:34:00 +0000

    confi

--- src/app.js
+++ src/app.js
@@ -12,10 +12,10 @@ window.onload = function() {
   let dominios = [".io", ".com", ".es"];
   console.log("Hello Rigo from the console!");
 
-  for (i = 0; i < pronombres.length; i++) {
-    for (i = 0; i < adjetivos.length; i++) {
-      for (i = 0; i < sustantivos.length; i++) {
-        for (i = 0; i < dominios.length; i++) {
+  for (let i = 0; i < pronombres.length; i++) {
+    for (let j = 0; j < adjetivos.length; j++) {
+      for (let k = 0; k < sustantivos.length; k++) {
+        for (let h = 0; h < dominios.length; h++) {
           console.log(`${pronombres},${adjetivos},${sustantivos},${dominios}`);
         }
       }
